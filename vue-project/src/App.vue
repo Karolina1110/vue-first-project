fd514b77b213056e934ef7948ef2b1eb48839557
Karolina1110 <karolinatujaka99@gmail.com>
2026-03-11 21:25:06 +0100

    added link to title

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,11 +6,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand">Account News</a>
+    <a class="navbar-brand"><router-link class="nav-link" to="/">Account News</router-link></a>
 
     <div class="navbar-nav">
-      <RouterLink class="nav-link" to="/">Home</RouterLink>
-      <RouterLink class="nav-link" to="/accounts">Accounts</RouterLink>
+      <router-link class="nav-link" to="/">Home</router-link>
+      <router-link class="nav-link" to="/accounts">Accounts</router-link>
     </div>
   </div>
 </nav>
